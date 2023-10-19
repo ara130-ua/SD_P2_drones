2c5a5e0165f450e2d98dce20389f393e85b2c23b
50506576W Adrián Ríos Armero <ara130@alu.ua.es>
2023-10-19 16:08:09 +0200

    manejo del fichero de entrada

--- AD_Engine.py
+++ AD_Engine.py
@@ -2,21 +2,63 @@ import socket
 import threading
 import sys
 import sqlite3
+import datetime
 
 #def consulta_Clima():
     
 HEADER = 64
-PORT = 5050
 FORMAT = 'utf-8'
+
+# Manejo de ficheros
+def file_manipulation(name, mode):
+  try:
+    file = open(name, mode)
+    return file
+  except OSError as err:
+    print("Error: {0}".format(err))
+  return
+
+def manejoFichero(maxDrones):
+   
+    fileWrite = 'figura.txt'
+    datos_drones = []
+
+   # Una manera de sacar el contenido en una string
+   #f = file_manipulation(fileWrite, 'r')
+   #contenido = f.read()
+   
+   # sacamos la información en lineas de los drones
+    with open(fileWrite, 'r') as archivo:
+        lineas = archivo.readlines()
+
+    for linea in lineas:
+      #Eliminamos espacios en blanco
+        linea = linea.strip()
+        if linea:
+            elementos = linea.split()
+
+            if len(elementos) == 3:
+                datos_drones.append((int(elementos[0]), float(elementos[1]), float(elementos[2])))            
+
+   # cerramos el fichero
+    return(datos_drones)
+
 
 ####main#####
 print ("Bienvenido al AD_Engine")
 
 
 #usaremos 6 argumentos, la BBDD no necesita de conexion
+# Puerto de escucha del engine
+# número máximo de drones
+# IP y puerto del Broker
+# IP y puerto del AD_Wheather
 if  (len(sys.argv) == 6):
     PORT = int(sys.argv[1])
-    MAX_DRONES = int(sys.argv[2])
+    SERVER = socket.gethostbyname(socket.gethostbyname())
+    ADDR = (SERVER,PORT)
+
+    manejoFichero(MAX_DRONES = int(sys.argv[2]))
 
     IP_BROKER = sys.argv[3]
     PORT_BROKER = int(sys.argv[4])
--- README.md
+++ README.md
@@ -3,3 +3,18 @@ Sistemas distribuidos práctica de drones
 
 **links de interes**
 https://andres-plazas.medium.com/leer-y-escribir-datos-en-kafka-usando-python-2696154c3948
+
+Paso a paso de la práctica:
+
+El engine entra en modo de escucha, hasta que un fichero le dará la información de la figura con un formato concreto,
+en este fichero encontramos la cantidad de drones que hay que desplegar y las coordenadas de cada pixel.
+
+Para desplegar los drones, una de las soluciones puede ser, desplegar 100 drones que es el máximo de ids que tenemos disponibles, e intentar acceder a todos con el engine, y que cuando el engine los rechace, matar esos procesos.
+
+Para el movimiento de los drones, podemos buscar un algoritmo de recorrido que lo haga en el menor número de movimientos, o lo podemos hacer de manera aleatoria, en la práctica no especifica ningún tipo de movimiento.
+
+Manejo de ficheros python
+https://tecnops.es/tutorial-de-python-parte-4/
+
+chatGPT información del fichero
+https://chat.openai.com/c/2ff0a61f-7550-4bb3-81a4-60c34c7fffe8
